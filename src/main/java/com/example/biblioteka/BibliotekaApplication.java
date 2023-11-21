e468c6f241c5bc4469b42f3fcca359116d592f1a
AniaWoj <wojtalewicz.ania@gmail.com>
2023-11-21 18:59:10 +0100

    Refactor

--- src/main/java/com/example/biblioteka/BibliotekaApplication.java
+++ src/main/java/com/example/biblioteka/BibliotekaApplication.java
@@ -11,20 +11,3 @@ public class BibliotekaApplication {
     }
 
 }
-/*
-TODO: 07.06.2023
-    1. Logowanie:
-        - przechowywanie zalogowanego użytkownika
-        - pobieranie danych do logowania z bazy
-    2. Dodać role użytkownikom, filtrować opcje po rolach użytkowników
-    3. Oddzielić Menu do osobnego nav.html i ładować na każdej podstronie
-        - ładować manu na podstawie posiadanych opcji?
-    4. Wypożyczanie książek:
-        - dodać do BookEntity informacje o wypożyczeniu
-    5. dołączyć API? do strony głównej?
-    6. Slider? wyświetlanie wyróżnionych książek...
-    7. api księgarni?
-    8. testy
-
-
- */
